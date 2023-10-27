3e353a9f765e052e50fa7da0b7d7256d170f5b67
Ethan Davis <edavis@bsd220.org>
2023-10-27 09:31:48 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Ethan Davis on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
